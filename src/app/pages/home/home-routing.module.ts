e8af19615b47d2d113874b0503bc779ae62fda49
ED1FF <alekarp11@gmail.com>
2024-07-28 12:52:16 +0300

    Add Reviews component

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -2,11 +2,25 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
+import { ReviewsPage } from './reviews/reviews.page';
 
 const routes: Routes = [
   {
     path: '',
     component: HomePage,
+    children: [
+      {
+        path: 'reviews',
+        component: ReviewsPage,
+        loadChildren: () =>
+          import('./reviews/reviews.module').then((m) => m.ReviewsPageModule),
+      },
+      {
+        path: '',
+        redirectTo: '/reviews',
+        pathMatch: 'full',
+      },
+    ],
   },
 ];
 
