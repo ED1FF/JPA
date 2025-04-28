d9dbb952332bd4d1e9101bd2253e9bb00484acbf
ED1FF <alekarp11@gmail.com>
2025-04-28 19:20:16 +0300

    update angular

--- src/app/pages/home/reviews/reviews.page.ts
+++ src/app/pages/home/reviews/reviews.page.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-reviews',
-  templateUrl: './reviews.page.html',
-  styleUrls: ['./reviews.page.scss'],
+    selector: 'app-reviews',
+    templateUrl: './reviews.page.html',
+    styleUrls: ['./reviews.page.scss'],
+    standalone: false
 })
 export class ReviewsPage implements OnInit {
 
